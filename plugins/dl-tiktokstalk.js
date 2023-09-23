3aae7d525e4b052e743727aa714fc2782c55bb12
kingxteamofc <145257148+kingxteamofc@users.noreply.github.com>
2023-09-23 16:57:54 +0530

    Add files via upload

---
+++ plugins/dl-tiktokstalk.js
@@ -0,0 +1,23 @@
+
+import fg from 'api-dylux'
+let handler = async (m, { conn, text, args }) => {
+	
+  if (!text) throw `✳️Enter the Username of a TikTok user `
+  let res = await fg.ttStalk(args[0])
+  let txt = `
+┌──「 *TIKTOK STALK* 
+✰ *🔖Number:* ${res.name}
+✰ *🔖Username:* ${res.username}
+✰ *👥followers:* ${res.followers}
+✰ *🫂following:* ${res.following}
+✰ *📌Desc:* ${res.desc}
+
+✰ *🔗 Link* : https://tiktok.com/${res.username}
+└────────────`
+  await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
+}
+handler.help = ['tiktokstalk']
+handler.tags = ['dl']
+handler.command = /^t(tstalk|iktokstalk)$/i
+
+export default handler
---
+++ plugins/dl-yts.js
@@ -0,0 +1,31 @@
+
+import yts from 'yt-search'
+
+let handler = async (m, {conn, text }) => {
+  if (!text) throw '🎯 What Do You Want To Search On YouTube?'
+  let results = await yts(text)
+  let tes = results.all
+  let teks = results.all.map(v => {
+    switch (v.type) {
+      case 'video': return `
+✭ ${v.title}
+✭ *Link* : ${v.url}
+✭ *Duration* : ${v.timestamp}
+✭ *publiced :* ${v.ago}
+✭ *Views:* ${v.views}
+
+   `.trim()
+      case 'canal': return `
+✭ *${v.name}* (${v.url})
+✭${v.subCountLabel} (${v.subCount}) Suscribe
+✭ ${v.videoCount} videos
+`.trim()
+    }
+  }).filter(v => v).join('\n\n________________________\n\n')
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
+}
+handler.help = ['ytsearch'] 
+handler.tags = ['dl']
+handler.command = ['ytsearch', 'yts'] 
+
+export default handler
